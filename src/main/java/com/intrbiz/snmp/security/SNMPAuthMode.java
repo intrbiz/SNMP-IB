7655569ddecf7cc47f59c8154c5824301066a5e2
Chris Ellis <chris@intrbiz.com>
2014-07-13 01:18:14 +0100

    Add SNMP auth and priv mode parse method

--- src/main/java/com/intrbiz/snmp/security/SNMPAuthMode.java
+++ src/main/java/com/intrbiz/snmp/security/SNMPAuthMode.java
@@ -1,5 +1,13 @@
 package com.intrbiz.snmp.security;
 
 public enum SNMPAuthMode {
-    NONE, MD5, SHA1
+    NONE, MD5, SHA1;
+    
+    public static SNMPAuthMode parse(String mode)
+    {
+        if ("none".equalsIgnoreCase(mode)) return NONE;
+        if ("md5".equalsIgnoreCase(mode)) return MD5;
+        if ("sha1".equalsIgnoreCase(mode) || "sha".equalsIgnoreCase(mode)) return SHA1;
+        return null;
+    }
 }
--- src/main/java/com/intrbiz/snmp/security/SNMPPrivMode.java
+++ src/main/java/com/intrbiz/snmp/security/SNMPPrivMode.java
@@ -1,5 +1,13 @@
 package com.intrbiz.snmp.security;
 
 public enum SNMPPrivMode {
-    NONE, AES128, DES
+    NONE, AES128, DES;
+    
+    public static SNMPPrivMode parse(String mode)
+    {
+        if ("none".equalsIgnoreCase(mode)) return NONE;
+        if ("aes128".equalsIgnoreCase(mode) || "aes".equalsIgnoreCase(mode)) return AES128;
+        if ("des".equalsIgnoreCase(mode)) return DES;
+        return null;
+    }
 }
